8faf21e544b6ad83d7192757a416178b7b976e1e
slisaasquatch <sli@saasquat.ch>
2024-07-04 11:42:16 -0700

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import com.google.pubsub.v1.PubsubMessage;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    PubsubMessage.newBuilder();
+  }
+}
